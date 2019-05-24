fa7ad2bdabd92e76376b405fa084655913ad9470
Przemek <przemyslaw.d.brzezinski@gmail.com>
2019-05-24 21:57:45 +0200

    add joda time

--- bin/create_native_image.sh
+++ bin/create_native_image.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 mvn clean package
-native-image -jar ./target/jb-hello-world-maven-0.1.0-jar-with-dependencies.jar hellworld
-time java -jar ./target/jb-hello-world-maven-0.1.0-jar-with-dependencies.jar
-time ./helloworld
+native-image -H:ReflectionConfigurationFiles=./reflection.json --no-server -jar ./target/jb-hello-world-maven-0.1.0-jar-with-dependencies.jar hellworld
