14b7ff726a72901e6d077a69c6adf86843e121c7
Przemek <przemyslaw.d.brzezinski@gmail.com>
2019-05-24 22:36:28 +0200

    resources

--- bin/create_native_image.sh
+++ bin/create_native_image.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 mvn clean package
-native-image -H:ReflectionConfigurationFiles=./reflection.json -H:IncludeResources=org/joda/time/tz/data/ZoneInfoMap --no-server -jar ./target/jb-hello-world-maven-0.1.0-jar-with-dependencies.jar hellworld
+native-image -H:ReflectionConfigurationFiles=./reflection.json -H:IncludeResources=./resources.json --no-server -jar ./target/jb-hello-world-maven-0.1.0-jar-with-dependencies.jar hellworld
