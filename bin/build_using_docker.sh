06d6306a35fa4ae2886972a970e9f0514699e6c3
solveretur <solveretur@github.com>
2019-05-25 19:09:34 +0200

    build by docker v1

--- bin/build_using_docker.sh
+++ bin/build_using_docker.sh
@@ -2,18 +2,16 @@
 echo "Starting creating native image..."
 #mvn clean package
 echo "Finished building app. Creating native image directory..."
+rm -rf ./build
+mkdir -p ./build
 docker run --rm --name graal -v $(pwd):/working oracle/graalvm-ce:latest \
-/bin/bash -c "native-image \
+sh -c "gu install native-image; \ 
+		    native-image \
                     -H:ReflectionConfigurationFiles=/working/reflection.json \
                     -H:ResourceConfigurationFiles=/working/resources.json \
-                    -H:+ReportUnsupportedElementsAtRuntime --no-server -jar /working/target/jb-hello-world-maven-0.1.0-jar-with-dependencies.jar hellworld \
+                    -H:+ReportUnsupportedElementsAtRuntime --no-server -jar /working/target/jb-hello-world-maven-0.1.0-jar-with-dependencies.jar nat_image \
                     ; \
-                    cp hellworld /working/build/server"
-
-echo "Copying native image from graalvm container..."
-rm -rf build/package
-mkdir build/package
-
-cp build/server build/package
+                    chmod 755 nat_image; \
+                    cp nat_image /working/build"
 
 echo "Native image creation finished"
