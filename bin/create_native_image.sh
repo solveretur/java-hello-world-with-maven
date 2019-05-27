1d2e59be11d8bc9a7ba19941b603c090cf7c3375
solveretur <solveretur@github.com>
2019-05-27 19:55:10 +0200

    improve scripts

--- bin/create_native_image.sh
+++ bin/create_native_image.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
+rm -rf ./build
+mkdir -p ./build
 mvn clean package
-native-image -H:ReflectionConfigurationFiles=./reflection.json -H:ResourceConfigurationFiles=./resources.json --no-server -jar ./target/jb-hello-world-maven-0.1.0-jar-with-dependencies.jar hellworld
+native-image -H:ReflectionConfigurationFiles=./reflection.json -H:ResourceConfigurationFiles=./resources.json --no-server -jar ./target/jb-hello-world-maven-0.1.0-jar-with-dependencies.jar nat_image
+mv nat_image ./build
--- bin/test.sh
+++ bin/test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 time java -jar ./target/jb-hello-world-maven-0.1.0-jar-with-dependencies.jar
-time ./hellworld
+time ./build/nat_image
