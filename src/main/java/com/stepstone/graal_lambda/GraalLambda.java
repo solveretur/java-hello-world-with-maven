ca4bb422c7b4116ba7bd00f0b5fd1186a9e40257
solveretur <solveretur@github.com>
2019-05-28 16:32:50 +0200

    release connection

--- src/main/java/com/stepstone/graal_lambda/GraalLambda.java
+++ src/main/java/com/stepstone/graal_lambda/GraalLambda.java
@@ -18,19 +18,20 @@ public class GraalLambda {
     private static final String EVENT_RESPONSE = "http://%s/2018-06-01/runtime/invocation/%s/response";
 
     public static void main(String[] args) {
+        final String awsLambdaRuntimeApi = System.getenv("AWS_LAMBDA_RUNTIME_API");
+        if (awsLambdaRuntimeApi == null) {
+            throw new RuntimeException("Couldn't solve host:");
+        }
+        final String nextEventEndpoint = String.format(NEXT_EVENT_ENDPOINT, awsLambdaRuntimeApi);
+        System.out.println("Listening endpoint on: " + nextEventEndpoint);
+        final HttpClient client = HttpClientBuilder.create().build();
         while (true) {
-            final String awsLambdaRuntimeApi = System.getenv("AWS_LAMBDA_RUNTIME_API");
-            if (awsLambdaRuntimeApi == null) {
-                throw new RuntimeException("Couldn't solve host:");
-            }
-            final String nextEventEndpoint = String.format(NEXT_EVENT_ENDPOINT, awsLambdaRuntimeApi);
-            System.out.println("Listening endpoint on: " + nextEventEndpoint);
-            final HttpClient client = HttpClientBuilder.create().build();
             final HttpGet nextEventRequest = new HttpGet(nextEventEndpoint);
             final EchoService echoService = new EchoService();
             try {
                 final HttpResponse response = client.execute(nextEventRequest);
                 System.out.println("Retrieved response: " + response.toString());
+                nextEventRequest.releaseConnection();
                 final Header[] allHeaders = response.getAllHeaders();
                 final String requestId = Arrays.stream(allHeaders)
                         .filter(h -> AWS_LAMBDA_RUNTIME_AWS_REQUEST_ID.equals(h.getName()))
@@ -46,6 +47,7 @@ public class GraalLambda {
                 httpPost.setEntity(new StringEntity(lambdaResponseBody));
                 final HttpResponse lambdaResponseResponse = client.execute(httpPost);
                 System.out.println("Retrieved response response: " + lambdaResponseResponse.toString());
+                httpPost.releaseConnection();
             } catch (final IOException e) {
                 e.printStackTrace();
                 throw new RuntimeException("Couldn't get next event");
