8421c2642e2dc9dfafbb619ff612dc4da72eef00
solveretur <solveretur@github.com>
2019-05-28 11:51:48 +0200

    lambda v1

--- bin/create_native_image.sh
+++ bin/create_native_image.sh
@@ -2,7 +2,7 @@
 rm -rf ./build
 mkdir -p ./build
 mvn clean package
-native-image -H:ReflectionConfigurationFiles=./reflection.json -H:ResourceConfigurationFiles=./resources.json --no-server -jar ./target/jb-hello-world-maven-0.1.0-jar-with-dependencies.jar nat_image
+native-image -H:ReflectionConfigurationFiles=./reflection.json -H:ResourceConfigurationFiles=./resources.json -Djava.net.preferIPv4Stack=true --no-server --enable-http --enable-https --enable-url-protocols=http,https --enable-all-security-services -jar ./target/jb-hello-world-maven-0.1.0-jar-with-dependencies.jar nat_image
 mv nat_image ./build
 cp ./runtime/bootstrap ./build/bootstrap
 cd ./build
