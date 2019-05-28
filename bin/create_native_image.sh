c55efa03ee37bc986071ccca75f38f4867883488
solveretur <solveretur@github.com>
2019-05-28 09:15:33 +0200

    create zip for lambda

--- bin/create_native_image.sh
+++ bin/create_native_image.sh
@@ -4,3 +4,8 @@ mkdir -p ./build
 mvn clean package
 native-image -H:ReflectionConfigurationFiles=./reflection.json -H:ResourceConfigurationFiles=./resources.json --no-server -jar ./target/jb-hello-world-maven-0.1.0-jar-with-dependencies.jar nat_image
 mv nat_image ./build
+cp ./runtime/bootstrap ./build/bootstrap
+cd ./build
+chmod +x ./nat_image
+chmod +x ./bootstrap
+zip runtime.zip nat_image bootstrap
